2fd06167c38d85d19b4faef6f56e16d0aca991b9
Luna <luna@stor.lol>
2017-10-13 01:31:41 +1000

    start on UpdateSeriesFromSonarr() func

--- utils/series_update.go
+++ utils/series_update.go
@@ -1,9 +1,23 @@
 package utils
 
 import (
-	"github.com/ileyd/topaz/utils/sonarr"
+	"github.com/ileyd/topaz/models"
+	"github.com/ileyd/topaz/sonarr"
 )
 
-func UpdateSeriesFromSonarr() {
-	sonarr.a
+func UpdateSeriesFromSonarr() (err error) {
+	series, err := sonarr.GetClient().GetAllSeries()
+	if err != nil {
+		return err
+	}
+
+	for _, s := range series {
+		episodes := make(map[int]map[int]models.Media)
+
+		episodeFiles, err := sonarr.GetClient().GetAllEpisodeFiles(s.ID)
+		if err != nil {
+			return err
+		}
+
+	}
 }
