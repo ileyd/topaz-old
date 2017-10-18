52e8603f59577e0963e69a8b53eb2af651939731
Luna <luna@stor.lol>
2017-10-19 04:18:13 +1000

    fix execution order issue

--- db.go
+++ db.go
@@ -2,27 +2,37 @@ package main
 
 import (
 	"crypto/tls"
+	"log"
 	"net"
+	"time"
 
 	"gopkg.in/mgo.v2"
 )
-
-var dbInfo = config.DB.DialInfo()
 
 var dbSession *mgo.Session
 var db *mgo.Database
 
 // initDb initialises the dbSession and db variables with a new mongodb session and the default database
-func initDb() {
-	tlsConfig := &tls.Config{}
+func initDb() (err error) {
+	var dbInfo = &mgo.DialInfo{
+		Addrs:          config.DB.Addrs,
+		ReplicaSetName: config.DB.ReplicaSetName,
+		Database:       config.DB.AuthDB,
+		Username:       config.DB.Username,
+		Password:       config.DB.Password,
+		Timeout:        time.Second * 5,
+	}
+
 	dbInfo.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+		tlsConfig := &tls.Config{}
 		conn, err := tls.Dial("tcp", addr.String(), tlsConfig)
 		return conn, err
 	}
 
-	dbSession, err := mgo.DialWithInfo(&dbInfo)
-	if err != nil {
-		panic(err)
+	dbSession, err = mgo.DialWithInfo(dbInfo)
+	if err == nil {
+		log.Println("successfully connected to the database")
 	}
-	db = dbSession.DB(config.DB.DefaultDB)
+	db = dbSession.DB(config.DB.AppDB)
+	return err
 }
