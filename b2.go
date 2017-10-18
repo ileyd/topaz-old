4dbe34f0a37752b29e7f0684cf5df93040a54523
Luna <luna@stor.lol>
2017-10-19 04:17:44 +1000

    add more logging and error handling

--- b2.go
+++ b2.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"net/url"
-	"os"
 	"strings"
 )
 
 // GenerateB2URL generates a streamable Backblaze B2 URL under the assumption that disk paths directly correlate to B2 paths, based on a given number of path components to strip
 func GenerateB2URL(absPath string) (*url.URL, error) {
-	pathComponents := strings.Split(absPath, string(os.PathSeparator))
-	strippedComponents := config.B2.StrippedComponents
-	relPath := strings.Join(pathComponents[(2*strippedComponents):], string(os.PathSeparator))
+	// NOTE: string(os.PathSeparator) makes more sense but in dev I'm running the sonarr server on a Linux machine while developing on a windows one
+	pathComponents := strings.Split(absPath, "/")
+	strippedComponents := 1 + config.B2.StrippedComponents
+	if len(pathComponents) < strippedComponents {
+		log.Println("GenerateB2URL encountered a path too short to handle", pathComponents)
+		return nil, errors.New("encountered a path too short to handle")
+	}
+	// see previous note
+	relPath := strings.Join(pathComponents[strippedComponents:], "/")
 
 	// very bad way of unescaping the path separators
 	urlRelPath := strings.Replace(url.QueryEscape(relPath), "%2F", "/", -1)
--- sonarr.go
+++ sonarr.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/ileyd/sonarr"
 )
 
@@ -9,5 +11,8 @@ var sonarrClient *sonarr.SonarrClient
 // initSonarrClient initialises the sonarrClient variable with a new sonarr client
 func initSonarrClient() (err error) {
 	sonarrClient, err = sonarr.NewSonarrClient(config.Sonarr.Addr, config.Sonarr.APIKey)
+	if err == nil {
+		log.Println("successfully connected to Sonarr")
+	}
 	return err
 }
